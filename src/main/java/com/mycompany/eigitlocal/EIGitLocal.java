fb73e41ca4763ebbb040d57ab22ed778230635ce
USER <USER@DESKTOP-E1BDAD6>
2025-04-24 19:38:37 -0500

    primer cambio

---
+++ src/main/java/com/mycompany/eigitlocal/EIGitLocal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+La POO, o Programación Orientada a Objetos, es un paradigma de programación que organiza el código en objetos, los cuales contienen datos (atributos) y funciones (métodos). Este enfoque permite crear software más modular, flexible y reutilizable. 
+Conceptos clave de la POO:
+Clases: Plantillas para crear objetos.
+Objetos: Instancias de las clases, con sus propios datos y funciones.
+Atributos: Datos que un objeto contiene.
+Métodos: Funciones que un objeto puede realizar.
+Abstracción: Ocultar detalles de implementación innecesarios.
+Encapsulación: Agrupar datos y métodos dentro de un objeto, restringiendo el acceso externo.
+Herencia: Un objeto puede heredar propiedades y métodos de otros objetos.
+Polimorfismo: Un objeto puede tomar diferentes formas dependiendo del contexto. 
+Ventajas de la POO:
+Reutilización de código: El código se puede reutilizar en diferentes partes de un proyecto o en otros proyectos. 
+Modularidad: El código se puede organizar en módulos independientes, facilitando la comprensión y el mantenimiento. 
+Flexibilidad: La POO permite adaptar el código a nuevas necesidades más fácilmente. 
+Organización: El código se puede organizar de forma más clara y lógic
